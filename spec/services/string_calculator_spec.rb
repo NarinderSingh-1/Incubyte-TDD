5d70d5f68156daabdc2d720a81f35fb83ba43da7
Narinder Singh <narindersingh@Narinders-MacBook-Air.local>
2024-09-01 01:09:14 +0530

    Write further Test cases for below points: 1. Single 2. Multiple 3. New Line 4. Delimeter 5. Negative Numbers

--- spec/services/string_calculator_spec.rb
+++ spec/services/string_calculator_spec.rb
@@ -8,5 +8,35 @@ RSpec.describe StringCalculator do
     it 'returns 0 for an empty string' do
       expect(StringCalculator.add('')).to eq(0)
     end
+
+    it 'returns the number itself when only one number is provided' do
+      expect(StringCalculator.add('5')).to eq(5)
+    end
+
+    it 'returns the sum of two numbers' do
+      expect(StringCalculator.add('1,2')).to eq(3)
+    end
+
+    it 'handles any amount of numbers' do
+      expect(StringCalculator.add('1,4,2,5,3,6')).to eq(21)
+    end
+
+    it 'handles new lines between numbers (instead of commas)' do
+      expect(StringCalculator.add("1\n2,3")).to eq(6)
+    end
+
+    it 'supports different delimiters' do
+      expect(StringCalculator.add("//;\n1;2")).to eq(3)
+    end
+
+    context 'negative numbers' do
+      it 'raises an exception for negative numbers' do
+        expect { StringCalculator.add('1,-2,3') }.to raise_error('negative numbers not allowed: -2')
+      end
+
+      it 'shows all negative numbers in the exception message' do
+        expect { StringCalculator.add('1,-2,-3') }.to raise_error('negative numbers not allowed: -2, -3')
+      end
+    end
   end
 end
